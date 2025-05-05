316b8dad21fbae6e67509211cdd111de42fe222f
Tadxss <tadxss@Daryl-Macbook-Pro-M4-Space-Black.local>
2025-05-06 03:41:57 +0800

    Fix tasks on one Project should not be seen on other project.

--- src/App.jsx
+++ src/App.jsx
@@ -92,17 +92,22 @@ function App() {
         };
     });
   }
+
   const selectedProject = projectsState.projects.find(
     project => project.id === projectsState.selectedProjectId
   );
   
+  const selectedProjectTasks = projectsState.tasks.filter(
+    task => task.projectId === projectsState.selectedProjectId
+  );
+
   let content = (
     <SelectedProject 
       project={selectedProject} 
       onDelete={handleDeleteProject} 
       onAddTask={handleAddTask}
       onDeleteTask={handleDeleteTask}
-      tasks={projectsState.tasks}
+      tasks={selectedProjectTasks}
     />
   );
 
